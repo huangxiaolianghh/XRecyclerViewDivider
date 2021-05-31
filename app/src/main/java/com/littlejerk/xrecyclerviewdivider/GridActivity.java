d18e57ec8340050ecdfcfb7b2f9e353046f02302
huanghuahong <huanghuahong@simuwang.com>
2021-05-31 18:42:43 +0800

    分割线调用例子初步

---
+++ app/src/main/java/com/littlejerk/xrecyclerviewdivider/GridActivity.java
@@ -0,0 +1,14 @@
+package com.littlejerk.xrecyclerviewdivider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+    }
+}
---
+++ app/src/main/java/com/littlejerk/xrecyclerviewdivider/LinearActivity.java
@@ -0,0 +1,14 @@
+package com.littlejerk.xrecyclerviewdivider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+    }
+}
