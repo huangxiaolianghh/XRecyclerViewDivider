c7fff75b275e7305624e9c479c0bb794a2a89a04
huangxiaolianghh <2550754753@qq.com>
2021-05-31 18:42:43 +0800

    分割线调用例子初步

--- app/src/main/java/com/littlejerk/xrecyclerviewdivider/MainActivity.java
+++ app/src/main/java/com/littlejerk/xrecyclerviewdivider/MainActivity.java
@@ -1,8 +1,11 @@
 package com.littlejerk.xrecyclerviewdivider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import static com.littlejerk.xrecyclerviewdivider.R.id.btn_StaggeredGridLayoutManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_LinearLayoutManager).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, LinearActivity.class));
+        });
+
+        findViewById(R.id.btn_GridLayoutManager).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, GridActivity.class));
+
+        });
+
+        findViewById(btn_StaggeredGridLayoutManager).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, StaggeredGridActivity.class));
+
+        });
+
     }
 }
